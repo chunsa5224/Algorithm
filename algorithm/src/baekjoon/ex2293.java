290bca37d82b39f0c830a6be3017e1b81be29df6
Heejin Son <chunsa5224@gmail.com>
2020-07-31 23:01:26 +0900

    Baekjoon DP 5 solved

---
+++ algorithm/src/baekjoon/ex2293.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ex2293 {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+
+}
