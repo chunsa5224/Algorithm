0399e21fdd168ec27e5721d04343624929cb9203
Heejin Son <chunsa5224@gmail.com>
2020-11-04 01:01:19 +0900

    11/04 Graph 1 solved

---
+++ algorithm/src/boj1613.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj1613 {
+//	Floyd Marshall
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		StringBuffer sb = new StringBuffer();
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int [][] map = new int [n+1][n+1];
+		
+		for(int i=0; i<k; i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			map[a][b] = -1;
+			map[b][a] = 1;
+		}
+		for(int l=1; l<=k; l++) {
+			for(int j=1; j<=k; j++) {
+				for(int i=1; i<=k; i++) {
+					if(map[j][l]==0 || map[i][l]!= map[l][j]) continue;
+					map[i][j]=map[i][l];
+				}
+			}
+		}
+		
+		int s = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<s; i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			int a =Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			sb.append(map[a][b]+"\n");
+		}
+		System.out.println(sb);
+	}
+
+}
